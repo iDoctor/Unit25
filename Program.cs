8e4643ded3603f9e01e0fa9b6f601339b446ca6b
Roman Filatov <ikardoc@gmail.com>
2022-08-19 22:21:45 +0300

    Added init DB Logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Unit25;
+using Unit25.Models;
+using AppContext = Unit25.AppContext;
+
+using (var db = new AppContext())
+{
+    var user1 = new User { Name = "Arthur", Email = "q@we.rty" };
+    var user2 = new User { Name = "Klim", Email = "a@sdf.gh" };
+
+    db.Users.Add(user1);
+    db.Users.Add(user2);
+    db.SaveChanges();
+}
