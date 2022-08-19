d0d12e17dfabe064d11f672f366e76c5c7c3214f
Roman Filatov <ikardoc@gmail.com>
2022-08-19 22:17:49 +0300

    Added DBContext

---
+++ AppContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Unit25.Models;
+
+namespace Unit25
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        // Объекты таблицы Books
+        public DbSet<Book> Books { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=localhost;Initial Catalog=Unit25_Library;Integrated Security=True");
+        }
+    }
+}
